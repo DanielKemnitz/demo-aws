3b6dbb26daa7a19baba45806f3411d144a4f7613
Daniel Kemnitz <daniel.kemnitz@atos.net>
2024-01-03 09:39:52 +0100

    pipelines & main.tf updated

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_elastic_beanstalk_application" "app" {
-  name = "demo-aws-app"
+  name = join("-", ["demo-aws-app",var.environment])
   description = "simple app to demonstrate DevOps workflows in AWS."
   tags = {
     owner = "daniel.kemnitz@atos.net"
@@ -21,7 +21,7 @@ resource "aws_elastic_beanstalk_application" "app" {
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name = "demo-aws-app-env"
+  name = join("-", ["demo-aws-app-env",var.environment])
   depends_on = [ aws_elastic_beanstalk_application.app ]
   application = aws_elastic_beanstalk_application.app.name
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.2 running Tomcat 10 Corretto 17"
